0f175c1eeb184b5a486472846ad95521cf0f32ee
ramyadav08 <ramrekhayadav412@gmail.com>
2023-08-22 19:15:48 +0530

    delete and post

--- class02/main.go
+++ class02/main.go
@@ -22,10 +22,56 @@ func getbook(c *gin.Context){
 	c.JSON(http.StatusOK,books)
 }
 
+func Createbook(c *gin.Context){
+	var book Book
+
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	books=append(books, book)
+
+	c.JSON(http.StatusCreated,book)
+
+}
+
+func GetBookById(c *gin.Context){
+	id:= c.Param("id")  // parameter sent by the client, then returns that book as a response.
+
+	for _, a:=range books{          // Loop over the list of albums, looking for
+		                            // an album whose ID value matches the parameter.
+		if a.ID==id{
+			c.IndentedJSON(http.StatusOK, a)
+            return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+}
+
+
+
+func DeleteBookById(c *gin.Context){
+	id:= c.Param("id")
+	 for i ,a:=range books{
+		if a.ID==id{
+			books = append(books[:i], books[i+1:]...)
+			break
+		}
+	 }
+	 c.JSON(http.StatusOK, gin.H{"message":"book deleted ???"})
+}
+
 func main() {
 	r := gin.Default()
 
 	r.GET("/",getbook)
+	r.POST("/create",Createbook)
+	r.GET("/books/:id", GetBookById)
+	r.DELETE("/books/:id",DeleteBookById)
 	r.Run(":5000")
 
 }
